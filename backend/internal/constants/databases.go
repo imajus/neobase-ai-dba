a96a9efd04803998e0f4809602390981b0fd0351
Bhaskar Kaura <bvkaura@gmail.com>
2025-02-16 22:02:04 +0530

    constants

--- backend/internal/constants/databases.go
+++ backend/internal/constants/databases.go
@@ -1,5 +1,7 @@
 package constants
 
+import "time"
+
 const (
 	DatabaseTypePostgreSQL = "postgresql"
 	DatabaseTypeMySQL      = "mysql"
@@ -8,3 +10,5 @@ const (
 	DatabaseTypeNeo4j      = "neo4j"
 	DatabaseTypeClickhouse = "clickhouse"
 )
+
+const DatabaseConnctionTTL = 10 * time.Minute // 10 minutes for database connection & schema
