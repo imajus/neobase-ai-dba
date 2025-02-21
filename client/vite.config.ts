4a2998c72784f2738aa9dae942667330e2963463
Bhaskar Kaura <bvkaura@gmail.com>
2025-02-22 00:51:59 +0530

    fix: uuid

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['uuid'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['uuid'],
+    },
   },
   publicDir: 'public',
   resolve: {
